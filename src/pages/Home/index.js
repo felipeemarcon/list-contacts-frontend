28d32f514caa54859ec00fadf7c950d3c59e9142
Felipe Marcon <felipeemarcon@gmail.com>
2019-11-19 20:51:18 -0300

    chore: add link to show all contacts in home

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,13 +1,12 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 // Style
-import "./index.scss";
+import "./style.scss";
 
 // Components
 import Content from "../../components/Content/Content";
 import Button from "../../components/Button/Button";
-
-// import { Container } from './styles';
 
 export default function Home(props) {
   const handleAddContact = () => {
@@ -20,6 +19,7 @@ export default function Home(props) {
         <p>Add a new contact to your list clicking in button bellow.</p>
         <Button action={handleAddContact} label="Add contact" />
       </Content>
+      <Link to="/contacts">Show all my contacts</Link>
     </div>
   );
 }
